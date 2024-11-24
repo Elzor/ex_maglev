d095a561e1c56bb15d5325b8dd1f8e14a268c60b
Maxim Molchanov <mmolchanov@vonmo.com>
2024-11-24 22:49:15 +0300

    setup version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExMaglev.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.0"
   @source_url "https://github.com/Vonmo/ex_maglev"
   @changelog_url "https://github.com/Vonmo/ex_maglev/blob/develop/CHANGELOG.md"
 
